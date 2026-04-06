7197efefd8031bd19c6bfb73c18199c8688e233e
Dylan Hatch <dylanbhatch@google.com>
2026-04-06 18:49:53 +0000

    sframe: Allow kernelspace sframe sections
    Generalize the sframe lookup code to support kernelspace sections. This
    is done by defining a SFRAME_LOOKUP option that can be activated
    separate from HAVE_UNWIND_USER_SFRAME, as there will be other client to
    this library than just userspace unwind.
    
    Sframe section location is now tracked in a separate sec_type field to
    determine whether user-access functions are necessary to read the sframe
    data. Relevant type delarations are moved and renamed to reflect the
    non-user sframe support.
    
    Signed-off-by: Dylan Hatch <dylanbhatch@google.com>

--- include/linux/sframe.h
+++ include/linux/sframe.h
@@ -3,29 +3,39 @@
 #define _LINUX_SFRAME_H
 
 #include <linux/mm_types.h>
+#include <linux/unwind_types.h>
 #include <linux/srcu.h>
-#include <linux/unwind_user_types.h>
 
-#ifdef CONFIG_HAVE_UNWIND_USER_SFRAME
+#ifdef CONFIG_UNWIND_SFRAME_LOOKUP
+
+enum sframe_sec_type {
+	SFRAME_KERNEL,
+	SFRAME_USER,
+};
 
 struct sframe_section {
-	struct rcu_head	rcu;
+	struct rcu_head  rcu;
 #ifdef CONFIG_DYNAMIC_DEBUG
-	const char	*filename;
+	const char		*filename;
 #endif
-	unsigned long	sframe_start;
-	unsigned long	sframe_end;
-	unsigned long	text_start;
-	unsigned long	text_end;
+	enum sframe_sec_type	sec_type;
+	unsigned long		sframe_start;
+	unsigned long		sframe_end;
+	unsigned long		text_start;
+	unsigned long		text_end;
 
-	unsigned long	fdes_start;
-	unsigned long	fres_start;
-	unsigned long	fres_end;
-	unsigned int	num_fdes;
+	unsigned long		fdes_start;
+	unsigned long		fres_start;
+	unsigned long		fres_end;
+	unsigned int		num_fdes;
 
-	signed char	ra_off;
-	signed char	fp_off;
+	signed char		ra_off;
+	signed char		fp_off;
 };
+
+#endif /* CONFIG_UNWIND_SFRAME_LOOKUP */
+
+#ifdef CONFIG_HAVE_UNWIND_USER_SFRAME
 
 #define INIT_MM_SFRAME .sframe_mt = MTREE_INIT(sframe_mt, 0),
 extern void sframe_free_mm(struct mm_struct *mm);
@@ -33,7 +43,6 @@ extern void sframe_free_mm(struct mm_struct *mm);
 extern int sframe_add_section(unsigned long sframe_start, unsigned long sframe_end,
 			      unsigned long text_start, unsigned long text_end);
 extern int sframe_remove_section(unsigned long sframe_addr);
-extern int sframe_find(unsigned long ip, struct unwind_user_frame *frame);
 
 static inline bool current_has_sframe(void)
 {
@@ -41,6 +50,8 @@ static inline bool current_has_sframe(void)
 
 	return mm && !mtree_empty(&mm->sframe_mt);
 }
+
+extern int sframe_find_user(unsigned long ip, struct unwind_frame *frame);
 
 #else /* !CONFIG_HAVE_UNWIND_USER_SFRAME */
 
@@ -52,8 +63,9 @@ static inline int sframe_add_section(unsigned long sframe_start, unsigned long s
 	return -ENOSYS;
 }
 static inline int sframe_remove_section(unsigned long sframe_addr) { return -ENOSYS; }
-static inline int sframe_find(unsigned long ip, struct unwind_user_frame *frame) { return -ENOSYS; }
 static inline bool current_has_sframe(void) { return false; }
+
+static inline int sframe_find_user(unsigned long ip, struct unwind_frame *frame) { return -ENOSYS; }
 
 #endif /* CONFIG_HAVE_UNWIND_USER_SFRAME */
 
