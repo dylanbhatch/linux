d3412b4c17b01dff21b3514adeb3d544615b8148
Dylan Hatch <dylanbhatch@google.com>
2026-04-28 18:36:39 +0000

    sframe: Provide PC lookup for vmlinux .sframe section
    With HAVE_UNWIND_KERNEL_SFRAME, read in the .sframe section at boot.
    This provides unwind data as an alternative/supplement to frame pointer
    based unwinding.
    
    Reviewed-by: Jens Remus <jremus@linux.ibm.com>
    Signed-off-by: Dylan Hatch <dylanbhatch@google.com>

--- include/linux/sframe.h
+++ include/linux/sframe.h
@@ -69,4 +69,18 @@ static inline int sframe_find_user(unsigned long ip, struct unwind_frame *frame)
 
 #endif /* CONFIG_HAVE_UNWIND_USER_SFRAME */
 
+#ifdef CONFIG_HAVE_UNWIND_KERNEL_SFRAME
+
+void __init init_sframe_table(void);
+void sframe_module_init(struct module *mod, void *sframe, size_t sframe_size,
+			void *text, size_t text_size);
+
+extern int sframe_find_kernel(unsigned long ip, struct unwind_frame *frame);
+
+#else
+
+static inline void __init init_sframe_table(void) {}
+
+#endif /* CONFIG_HAVE_UNWIND_KERNEL_SFRAME */
+
 #endif /* _LINUX_SFRAME_H */
